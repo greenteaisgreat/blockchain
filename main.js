167d0fbc8e1d7b3e355fb3c2922f2453e5d43259
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-03 11:34:05 -0600

    Add mineBlock Method & Nonce/Difficulty Properties

--- main.js
+++ main.js
@@ -6,19 +6,37 @@ class Block {
   //data: any kind of data you want associated with the block (amount of $, sender, receiver, etc)
   //prevHash: a string pointing to the previous block's hash, not the current;
   //the hash is what ensures the integrity of the entire chain
+  //nonce: a completely random value to help in mining blocks
   constructor(index, timestamp, data, prevHash = "") {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.prevHash = prevHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
     //crypto-js implementation of SHA256 cryptographic hash
     return SHA256(
-      this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)
+      this.index +
+        this.prevHash +
+        this.timestamp +
+        this.nonce +
+        JSON.stringify(this.data)
     ).toString();
+  }
+
+  //this method works off the principle of 'proof of work', where a lot of computing power is
+  //required to 'mine' a block, based on finding the correct # of 0's prepending a block's hash
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.hash = this.calculateHash();
+      this.nonce++;
+    }
+    console.log("Block mined: ", this.hash);
   }
 }
 
@@ -27,6 +45,7 @@ class Blockchain {
   //needs to be added manually
   constructor() {
     this.chain = [this.createGenesis()];
+    this.difficulty = 5;
   }
 
   createGenesis() {
@@ -41,8 +60,8 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.prevHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
-
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -60,14 +79,18 @@ class Blockchain {
 }
 
 const myChain = new Blockchain();
+
+console.log("Mining block 1...");
 myChain.addBlock(new Block(1, new Date().toLocaleDateString(), { amount: 4 }));
+console.log("Mining block 2...");
 myChain.addBlock(new Block(2, new Date().toLocaleDateString(), { amount: 7 }));
+console.log("Mining block 3...");
 myChain.addBlock(new Block(3, new Date().toLocaleDateString(), { amount: 2 }));
 
 //show the nicely formatted blockchain
 // console.log(JSON.stringify(myChain, null, myChain.chain.length));
 
-console.log(myChain.isChainValid());
+// console.log(myChain.isChainValid());
 
 //attempt to alter the existing blockchain & fail
 // myChain.chain[1].data = { amount: 100 };
